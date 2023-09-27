f4f0a3722d6bc42c0a58c6939d85e61520c16d8a
brokqwiks <120338990+brokqwiks@users.noreply.github.com>
2023-09-27 21:17:36 +0300

    Delete laravel directory

--- laravel/public/js/home_page.js
+++
@@ -1,6 +0,0 @@
-const href = document.getElementsByClassName("href-btn");
-document.getElementsByClassName("button").onmouseover = function()
-{   
-    this.style.backgroundColor = "black";
-    href.style.color = "white";
-}
