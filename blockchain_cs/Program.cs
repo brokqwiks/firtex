453273cd121390d36fea29838a044b63b1d6aea7
brokqwiks <brokqwiks@mail.ru>
2023-11-09 22:00:55 +0300

    blockchain_in_cs

---
+++ blockchain_cs/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string mnemonic_phrase = MnemonicGenerator.generate_phrase();
+        string privateKeyHexExample = PrivateKey.generate_private_key(mnemonic_phrase);
+        string publicKeyExample = PublicKey.GeneratePublicKey(privateKeyHexExample);
+        string address = AddressGenerator.GenerateReadableAddress(publicKeyExample);
+        Console.WriteLine(address);
+    }
+}
---
+++ blockchain_cs/public_key.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+class PublicKey
+{
+    public static string GeneratePublicKey(string privateKeyHex)
+    {
+        try
+        {
+            // Преобразование приватного ключа из шестнадцатеричной строки
+            byte[] privateKeyBytes = StringToByteArray(privateKeyHex);
+
+            // Создание объекта ECDSA
+            using (ECDsa ecdsa = ECDsa.Create())
+            {
+                // Установка параметров кривой и приватного ключа
+                ECParameters ecParameters = new ECParameters { Curve = ECCurve.NamedCurves.nistP256, D = privateKeyBytes };
+                ecdsa.ImportParameters(ecParameters);
+
+                // Получение публичного ключа в формате байтов
+                byte[] publicKeyBytes = ecdsa.ExportSubjectPublicKeyInfo();
+
+                // Преобразование публичного ключа в шестнадцатеричную строку
+                string publicKeyHex = BitConverter.ToString(publicKeyBytes).Replace("-", "").ToLower();
+
+                return publicKeyHex;
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+            return null;
+        }
+    }
+
+    // Вспомогательная функция для преобразования шестнадцатеричной строки в массив байтов
+    private static byte[] StringToByteArray(string hex)
+    {
+        int numberChars = hex.Length;
+        byte[] bytes = new byte[numberChars / 2];
+        for (int i = 0; i < numberChars; i += 2)
+        {
+            bytes[i / 2] = Convert.ToByte(hex.Substring(i, 2), 16);
+        }
+        return bytes;
+    }
+}
