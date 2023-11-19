71de385ead562342aaa92b03a4ba75e8b3c284a2
brokqwiks <brokqwiks@mail.ru>
2023-11-19 17:07:07 +0300

    Add blockchain_cs exe

---
+++ blockchain_cs/scr/cfg/configs.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Config
+{
+    public static void CreateMainConfig()
+    {
+        string folderPath = "cfg";
+        string filePath = Path.Combine(folderPath, "main.cfg");
+
+        // Ваши данные для записи
+        string dataToWrite = "[Global]\nType=Main";
+
+        try
+        {
+            // Проверяем, существует ли папка
+            if (!Directory.Exists(folderPath))
+            {
+                // Если папка не существует, создаем ее
+                Directory.CreateDirectory(folderPath);
+            }
+
+            // Используем StreamWriter для записи данных в файл
+            using (StreamWriter writer = new StreamWriter(filePath))
+            {
+                // Записываем данные в файл
+                writer.Write(dataToWrite);
+            }
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Ошибка при записи данных в файл: {ex.Message}");
+        }
+    }
+
+    public static string ReadMainConfig()
+    {
+        try
+        {
+            string filePath = "cfg\\main.cfg";
+            if (!File.Exists(filePath))
+            {
+                throw new FileNotFoundException("File not exist", filePath);
+            }
+
+            // Используем StreamReader для чтения данных из файла
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                // Читаем все содержимое файла
+                return reader.ReadToEnd();
+            }
+        }
+        catch (Exception ex)
+        {
+            throw new Exception($"Ошибка при чтении данных из файла: {ex.Message}");
+        }
+    }
+
+    public static void CreateCopyConfig(string fileName, string address)
+    {
+        string file = $"{fileName}.cfg";
+        string folderPath = "cfg";
+
+        string filePath = Path.Combine(folderPath, file);
+        string dataToWrite = $"[Global]\nType=Copy\nData=Wallet\n[Wallet]\nAddress={address}";
+
+        try
+        {
+            // Проверяем, существует ли папка
+            if (!Directory.Exists(folderPath))
+            {
+                // Если папка не существует, создаем ее
+                Directory.CreateDirectory(folderPath);
+            }
+
+            // Используем StreamWriter для записи данных в файл
+            using (StreamWriter writer = new StreamWriter(filePath))
+            {
+                // Записываем данные в файл
+                writer.Write(dataToWrite);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Ошибка при записи данных в файл: {ex.Message}");
+        }
+    }
+
+    public static void DeleteConfig(string fileName)
+    {
+        string filePath = $"{fileName}.cfg"; // Укажите полный путь к файлу .cfg
+
+        try
+        {
+            // Проверяем, существует ли файл
+            if (File.Exists(filePath))
+            {
+                // Если файл существует, удаляем его
+                File.Delete(filePath);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Ошибка при удалении файла: {ex.Message}");
+        }
+    }
+
+    public static string FindAddressInConfigs(string exeFileName)
+    {
+        string cfgFolderPath = "cfg";
+        string[] cfgFiles = Directory.GetFiles(cfgFolderPath, "*.cfg");
+
+        foreach (var cfgFile in cfgFiles)
+        {
+            string[] lines = File.ReadAllLines(cfgFile);
+
+            foreach (var line in lines)
+            {
+                // Проверяем, содержится ли "Address=" в строке конфига
+                if (line.Contains("Address="))
+                {
+                    // Если да, извлекаем значение после "Address="
+                    string addressValue = line.Split('=')[1].Trim();
+
+                    if (addressValue == exeFileName)
+                    {
+                        return addressValue;
+                    }
+                }
+            }
+        }
+
+        // Если ничего не найдено, возвращаем null
+        return null;
+    }
+}
---
+++ blockchain_cs/scr/exe.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+public class Exe
+{
+    public static void CreateCopyExe(string CopyName, string address)
+    {
+        string sourceFilePath = "blockchain_cs.exe";
+        string destinationFilePath = $"{CopyName}.exe";
+
+        try
+        {
+            File.Copy(sourceFilePath, destinationFilePath, true);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Ошибка при копировании файла: {ex.Message}");
+        }
+
+        Config.CreateCopyConfig(address, address);
+    }
+
+    public static void OpenCopyExe(string fileName)
+    {
+        string file = $"{fileName}.exe";
+        // Проверяем существование файла
+        if (File.Exists(file))
+        {
+            // Создаем процесс с настройками
+            ProcessStartInfo psi = new ProcessStartInfo
+            {
+                FileName = file,
+                UseShellExecute = true,
+                RedirectStandardOutput = false,
+                CreateNoWindow = false
+            };
+            // Запускаем новый exe файл
+            Process.Start(psi);
+
+            Thread.Sleep(600000);
+            // Завершаем текущий процесс (если нужно)
+            Environment.Exit(0);
+        }
+        else
+        {
+            Console.WriteLine("File not exist");
+        }
+    }
+
+    public static string GetFileName()
+    {
+        System.Diagnostics.Process currentProcess = System.Diagnostics.Process.GetCurrentProcess();
+
+        // Получаем полный путь к запускаемому exe-файлу
+        string exeFilePath = currentProcess.MainModule.FileName;
+
+        // Извлекаем только имя файла без пути и расширения
+        string exeFileName = Path.GetFileNameWithoutExtension(exeFilePath);
+
+        if (exeFileName != null)
+        {
+            return exeFileName;
+        }
+        else { return null; }
+    }
+
+    public static bool FindExeToAddress(string address)
+    {
+        // Завершаем существующий процесс, если он существует
+
+        string exeFilePath = $"{AppDomain.CurrentDomain.BaseDirectory}{address}.exe";
+
+        // Проверяем существует ли exe-файл
+        if (File.Exists(exeFilePath))
+        {
+            // Найден exe-файл, запускаем его в новом окне cmd
+            Console.WriteLine($"A wallet with the address {address} was found. Launch...");
+
+            ProcessStartInfo psi = new ProcessStartInfo
+            {
+                FileName = "cmd.exe",
+                RedirectStandardInput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true,
+            };
+
+            Process cmdProcess = new Process
+            {
+                StartInfo = psi
+            };
+            KillExistingProcess();
+            cmdProcess.Start();
+
+            // Передаем команду ввода в cmd
+            cmdProcess.StandardInput.WriteLine($"start {exeFilePath}");
+
+            // Завершаем функцию
+            return true;
+        }
+        else
+        {
+            // Файл не найден, выводим сообщение об ошибке
+            Console.WriteLine("Error: exe-файл не найден.");
+            return false;
+        }
+    }
+
+    private static void KillExistingProcess()
+    {
+        string exeFileName = $"{GetFileName()}.exe";
+        // Получаем все процессы с заданным именем
+        Process[] processes = Process.GetProcessesByName(exeFileName);
+
+        // Завершаем каждый процесс
+        foreach (Process process in processes)
+        {
+            process.Kill();
+            process.WaitForExit(); // Ждем, пока процесс завершится
+        }
+    }
+}
